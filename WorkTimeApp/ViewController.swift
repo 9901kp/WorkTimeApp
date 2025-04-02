21bf5611b18a17ecb246aabbd7ffce4848643e19
Kaipov Mukhammed Marsovich <117507679+9901kp@users.noreply.github.com>
2025-04-03 00:08:12 +0400

    first commit

---
+++ WorkTimeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkTimeApp
+//
+//  Created by Мухаммед Каипов on 3/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WorkTimeAppTests/WorkTimeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkTimeAppTests.swift
+//  WorkTimeAppTests
+//
+//  Created by Мухаммед Каипов on 3/4/25.
+//
+
+import Testing
+@testable import WorkTimeApp
+
+struct WorkTimeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
